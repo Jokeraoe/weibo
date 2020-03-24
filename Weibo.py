062301854182c2ef6ff38c9120bbc4bfacf0f742
Joker <397905195@qq.com>
2020-03-24 15:00:04 +0800

    add gui
    Signed-off-by: Joker <397905195@qq.com>

--- Weibo.py
+++ Weibo.py
@@ -1,7 +1,10 @@
-import requests 
+import requests
+import tkinter 
 from lxml import etree 
+from tkinter import Listbox
  
 url="https://s.weibo.com/top/summary?Refer=top_hot&topnav=1&wvr=6" 
+
 header={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'} 
 
 def get_trending(): 
@@ -14,9 +17,24 @@ def get_trending():
     top=affair[0] 
     affair=affair[1:]
     
-    print('{0:<10}\t{1:<40}'.format("top",top))
-    
+    Gui = tkinter.Tk()
+
+    title = Gui.title('微博热搜')
+
+    topnew = Listbox(Gui,height = 1,width = 40,font = '黑体',foreground= 'red')
+    hotnews = Listbox(Gui,height = 39 , width = 40,font = '黑体')
+
+    # print('{0:<10}\t{1:<40}'.format("top",top))
+    topnew.insert(0,'置顶:' + top)
+
+
     for i in range(0, len(affair)):
-        print("{0:<10}\t{1:{3}<30}\t{2:{3}>20}".format(rank[i],affair[i],view[i],chr(12288))) 
+        # print("{0:<10}\t{1:{3}<30}\t{2:{3}>20}".format(rank[i],affair[i],view[i],chr(12288))) 
+        hotnews.insert(i,'%d.'%(i+1) + affair[i])
+
+    topnew.pack()
+    hotnews.pack()
+
+    Gui.mainloop()
 
 get_trending()
